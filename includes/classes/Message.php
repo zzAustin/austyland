ac665301b3f2f6b00d9599d53331c4bf0795ac8b
Zhang Zhong <zhong.zhang@ubisoft.com>
2019-05-22 19:27:55 +0800

    march on

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -33,5 +33,47 @@
 				$query = mysqli_query($this->con,"INSERT INTO messages VALUES('','$user_to','$userLoggedIn','$body','$date','no','no','no')");
 			}
 		}
+
+		public function getMessages($otherUser){
+			$userLoggedIn = $this->user_obj->getUsername();
+			$data = "";
+			$query = mysqli_query($this->con, "UPDATE messages SET opened='yes' WHERE user_to='$userLoggedIn' AND user_from='$otherUser'");
+			$get_messages_query = mysqli_query($this->con, "SELECT * FROM messages WHERE (user_to='$userLoggedIn' AND user_from='$otherUser') OR (user_from='$userLoggedIn' AND user_to='$otherUser')");
+
+			while($row = mysqli_fetch_array($get_messages_query)){
+				$user_to = $row['user_to'];
+				$user_from = $row['user_from'];
+				$body = $row['body'];
+
+				$div_top = ($user_to == $userLoggedIn) ? "<div class='message' id='green'>":"<div class='message' id='blue'>";
+				$data = $data . $div_top . $body . "</div><br><br>";
+			}
+
+			return $data;
+		}
+
+		public function getLatestMessage($userLoggedIn, $username){
+			
+		}
+
+		public function getConvos(){
+			$userLoggedIn = $this->user_obj->getUsername();
+			$return_string = "";
+			$convos = array();
+			$query = mysqli_query($this->con,"SELECT user_to, user_from FROM messages WHERE user_to='$userLOggedIn' OR user_from='$userLoggedIn'");
+
+			while($row = mysqli_fetch_array($query)){
+				$user_to_push = ($row['user_to'] != $userLoggedIn) ? $row['user_to'] : $row['user_from'];
+
+				if(!in_array($user_to_push, $convos)){
+					array_push($convos, $user_to_push);
+				}
+			}
+
+			foreach($convos as $username){
+				$user_found_obj = new User($this->con, $username);
+				$latest_message_details = $this->getLatestMessage($userLoggedIn, $username);
+			}
+		}
 	}
 ?>
